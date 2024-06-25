aefcd96883aea1b9e66902e3ce0bb6ac96b11e91
erenKisaki <rakeshkisaki@gmail.com>
2024-06-25 22:58:30 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,3 +1,19 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class BulkUploadUsingExcel {
     private static Logger logger = LoggerFactory.getLogger(BulkUploadUsingExcel.class);
 
@@ -5,13 +21,16 @@ public class BulkUploadUsingExcel {
         logger.info("Starting Utility.....");
         try {
             String curlData = getCurlAsString();
-            Map<String, List<String>> dataMap = readDataFromExcel("C:\\Users\\ZKIAPMO.CORP\\Documents\\Excel Data Read\\SampleTestData.xlsx");
-            for (Map.Entry<String, List<String>> entry : dataMap.entrySet()) {
-                String selector = entry.getKey();
-                List<String> connectionIdList = entry.getValue();
-                callServiceUsingCurl(curlData, selector, connectionIdList);
-                logger.info(selector + ": " + connectionIdList);
-            }
+            Map<String, List<String>> samlDataMap = new HashMap<>();
+            Map<String, List<String>> oauthDataMap = new HashMap<>();
+            Map<String, List<String>> pingAccessDataMap = new HashMap<>();
+            
+            readDataFromExcel("C:\\Users\\ZKIAPMO.CORP\\Documents\\Excel Data Read\\SampleTestData.xlsx", samlDataMap, oauthDataMap, pingAccessDataMap);
+            
+            processData(curlData, samlDataMap, "SAML");
+            processData(curlData, oauthDataMap, "OAUTH");
+            processData(curlData, pingAccessDataMap, "PingAccess");
+            
         } catch (Exception e) {
             logger.debug("SelectorError: Error executing the script. Message: " + e.getMessage(), e);
             logger.error("SelectorError: Error executing the script. Message: " + e.getMessage());
@@ -30,11 +49,10 @@ public class BulkUploadUsingExcel {
         }
     }
 
-    public static Map<String, List<String>> readDataFromExcel(String path) {
-        Map<String, List<String>> dataMap = new HashMap<>();
+    public static void readDataFromExcel(String path, Map<String, List<String>> samlDataMap, Map<String, List<String>> oauthDataMap, Map<String, List<String>> pingAccessDataMap) {
         try (FileInputStream fis = new FileInputStream(path)) {
             Workbook wb = new XSSFWorkbook(fis);
-            Sheet sheet = wb.getSheetAt(0);
+            Sheet sheet = wb.getSheet("Authtype");
             Iterator<Row> iterator = sheet.iterator();
             while (iterator.hasNext()) {
                 Row currentRow = iterator.next();
@@ -43,89 +61,102 @@ public class BulkUploadUsingExcel {
                 }
                 Cell selectorCell = currentRow.getCell(0);
                 Cell connectionIdCell = currentRow.getCell(1);
-                if (selectorCell != null && connectionIdCell != null) {
+                Cell authTypeCell = currentRow.getCell(2);
+                if (selectorCell != null && connectionIdCell != null && authTypeCell != null) {
                     String selector = selectorCell.getStringCellValue();
                     String connectionId = connectionIdCell.getStringCellValue();
-                    if (!dataMap.containsKey(selector)) {
-                        dataMap.put(selector, new ArrayList<>());
+                    String authType = authTypeCell.getStringCellValue();
+                    
+                    switch (authType) {
+                        case "SAML":
+                            samlDataMap.computeIfAbsent(selector, k -> new ArrayList<>()).add(connectionId);
+                            break;
+                        case "OAUTH":
+                            oauthDataMap.computeIfAbsent(selector, k -> new ArrayList<>()).add(connectionId);
+                            break;
+                        case "PingAccess":
+                            pingAccessDataMap.computeIfAbsent(selector, k -> new ArrayList<>()).add(connectionId);
+                            break;
                     }
-                    dataMap.get(selector).add(connectionId);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return dataMap;
     }
 
-public static boolean callServiceUsingCurl(String curlCommand, String selector, List<String> connectionIdList) {
-		try {
-			List<String> matchList = splitCurl(curlCommand);
-			String[] parts = matchList.toArray(new String[0]);
-			String reqMethod = parts[2];
-			String url = parts[4].replaceAll("'", "").replace("SAMLPassthroughConnSetSel", selector);
-			StringBuilder headers = new StringBuilder();
-			String jsonData = "";
-			for (int i = 6; i < parts.length; i++) {
-				if (parts[i].startsWith("-H")) {
-					headers.append(parts[i + 1].replaceAll("\"", "").replaceAll("\n", "")).append("\n");
-					i++;
-				} else if (parts[i].startsWith("-d")) {
-					jsonData = parts[i + 1].replaceAll("\"", "");
-					i++;
-				}
-			}
-			
-			
-			List<List<String>> partitionedConnList = getPartitionedList(connectionIdList, 100);
-			for(List<String> connectIdList: partitionedConnList) {
-				URL urlobj = new URL(url);
-				HttpURLConnection connection = (HttpURLConnection) urlobj.openConnection();
-				connection.setRequestMethod(reqMethod);
-				connection.setDoOutput(true);
-				// Setting headers
-				String[] headerParts = headers.toString().split("\n");
-				for (String header : headerParts) {
-					String[] keyValue = header.split(": ");
-					connection.setRequestProperty(keyValue[0], keyValue[1]);
-				}
-				// Forming the Request body JSON Object
-				jsonData = modifyJsonData(jsonData, connectIdList, selector);
-				connection.getOutputStream().write(jsonData.getBytes());
-				int responseCode = connection.getResponseCode();
-				logger.info("The response code: " + responseCode);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+    public static void processData(String curlData, Map<String, List<String>> dataMap, String authType) {
+        for (Map.Entry<String, List<String>> entry : dataMap.entrySet()) {
+            String selector = entry.getKey();
+            List<String> connectionIdList = entry.getValue();
+            callServiceUsingCurl(curlData, selector, connectionIdList);
+            logger.info(authType + " - " + selector + ": " + connectionIdList);
+        }
+    }
 
-	private static String modifyJsonData(String jsonData, List<String> connectionIds, String selector)
-			throws Exception {
-		StringBuilder connectionsJson = new StringBuilder();
-		for (String connectionId : connectionIds) {
-			connectionsJson.append("[\"fields\": [[\"name\": \"Connection\", \"value\": \"").append(connectionId)
-					.append("\"]], \"defaultRow\": false},");
-		}
-		if (connectionsJson.length() > 0) {
-			connectionsJson.setLength(connectionsJson.length() - 1); // Remove trailing comma
-		}
-		jsonData = jsonData.replace("\"SAMLPassthroughConnSetSel\"", "\"" + selector + "\"").replaceAll("\n", "")
-				.replaceAll("\r", "");
-		JSONObject reqBodyJson = new JSONObject(jsonData);
-		if (reqBodyJson.has("configuration")) {
-			JSONObject configJson = reqBodyJson.getJSONObject("configuration");
-			String configData = configJson.toString();
-			configData = configData.replace("\"rows\": []", "\"rows\": [" + connectionsJson.toString() + "]");
-			reqBodyJson.put("configuration", new JSONObject(configData));
-		}
-		return reqBodyJson.toString();
-	}
+    public static boolean callServiceUsingCurl(String curlCommand, String selector, List<String> connectionIdList) {
+        try {
+            List<String> matchList = splitCurl(curlCommand);
+            String[] parts = matchList.toArray(new String[0]);
+            String reqMethod = parts[2];
+            String url = parts[4].replaceAll("'", "").replace("SAMLPassthroughConnSetSel", selector);
+            StringBuilder headers = new StringBuilder();
+            String jsonData = "";
+            for (int i = 6; i < parts.length; i++) {
+                if (parts[i].startsWith("-H")) {
+                    headers.append(parts[i + 1].replaceAll("\"", "").replaceAll("\n", "")).append("\n");
+                    i++;
+                } else if (parts[i].startsWith("-d")) {
+                    jsonData = parts[i + 1].replaceAll("\"", "");
+                    i++;
+                }
+            }
+            URL urlobj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) urlobj.openConnection();
+            connection.setRequestMethod(reqMethod);
+            connection.setDoOutput(true);
+            // Setting headers
+            String[] headerParts = headers.toString().split("\n");
+            for (String header : headerParts) {
+                String[] keyValue = header.split(": ");
+                connection.setRequestProperty(keyValue[0], keyValue[1]);
+            }
+            // Forming the Request body JSON Object
+            jsonData = modifyJsonData(jsonData, connectionIdList, selector);
+            connection.getOutputStream().write(jsonData.getBytes());
+            int responseCode = connection.getResponseCode();
+            logger.info("The response code: " + responseCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-	private static List<String> splitCurl(String curlCommand) {
+    private static String modifyJsonData(String jsonData, List<String> connectionIds, String selector)
+            throws Exception {
+        StringBuilder connectionsJson = new StringBuilder();
+        for (String connectionId : connectionIds) {
+            connectionsJson.append("[\"fields\": [[\"name\": \"Connection\", \"value\": \"").append(connectionId)
+                    .append("\"]], \"defaultRow\": false},");
+        }
+        if (connectionsJson.length() > 0) {
+            connectionsJson.setLength(connectionsJson.length() - 1); // Remove trailing comma
+        }
+        jsonData = jsonData.replace("\"SAMLPassthroughConnSetSel\"", "\"" + selector + "\"").replaceAll("\n", "")
+                .replaceAll("\r", "");
+        JSONObject reqBodyJson = new JSONObject(jsonData);
+        if (reqBodyJson.has("configuration")) {
+            JSONObject configJson = reqBodyJson.getJSONObject("configuration");
+            String configData = configJson.toString();
+            configData = configData.replace("\"rows\": []", "\"rows\": [" + connectionsJson.toString() + "]");
+            reqBodyJson.put("configuration", new JSONObject(configData));
+        }
+        return reqBodyJson.toString();
+    }
+
+    private static List<String> splitCurl(String curlCommand) {
         List<String> matchList = new ArrayList<>();
-        Pattern regex = Pattern.compile("('[^']')|(\"[^\"]\")|(\\S+)");
+        Pattern regex = Pattern.compile("('[^']*')|(\"[^\"]*\")|(\\S+)");
         Matcher regexMatcher = regex.matcher(curlCommand);
         while (regexMatcher.find()) {
             if (regexMatcher.group(1) != null) {
@@ -137,13 +168,5 @@ public static boolean callServiceUsingCurl(String curlCommand, String selector, 
             }
         }
         return matchList;
-    }
-
-	private static List<List<String>> getPartitionedList(List<String> origList, int partitionSize) {
-		List<List<String>> partitions = new ArrayList<>();
-		for (int i = 0; i < origList.size(); i += partitionSize) {
-			partitions.add(origList.subList(i, Math.min(i + partitionSize, origList.size())));
-		}
-		return partitions;
-	}
+    }
 }
