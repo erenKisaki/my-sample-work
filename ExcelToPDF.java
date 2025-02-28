52cb89e02c06e7bd8487ad3b8d9a3c29bdb2fd7c
erenKisaki <rakeshkisaki@gmail.com>
2025-02-28 18:01:10 +0530

    Update ExcelToPDF.java

--- ExcelToPDF.java
+++ ExcelToPDF.java
@@ -1,69 +1,55 @@
-import java.io.*;
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.ss.usermodel.*;
-import org.apache.pdfbox.pdmodel.*;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
-import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
-
-public class ExcelToPDF {
-    public static void main(String[] args) throws Exception {
-        byte[] excelData = getExcelByteArray(); // Load your XLS file as byte array
-
-        InputStream fs = new ByteArrayInputStream(excelData);
-        HSSFWorkbook workbook = new HSSFWorkbook(fs);
-        PDDocument pdf = new PDDocument();
+      PDDocument pdf = new PDDocument();
         PDPage page = new PDPage(PDRectangle.A4);
         pdf.addPage(page);
 
         PDPageContentStream contentStream = new PDPageContentStream(pdf, page);
         contentStream.setFont(PDType1Font.HELVETICA, 10);
         contentStream.setLeading(14);
+
         float margin = 50;
         float yStart = page.getMediaBox().getHeight() - margin;
         float tableWidth = page.getMediaBox().getWidth() - 2 * margin;
         float rowHeight = 20;
+        float yPosition = yStart;
         float tableX = margin;
 
         // Define column widths (adjust as needed)
         float[] colWidths = {70, 50, 90, 90, 80, 100, 60, 80, 80, 80, 100, 100};
         int numCols = colWidths.length;
-        float tableY = yStart;
+
+        FormulaEvaluator evaluator = workbook.getCreationHelper().createFormulaEvaluator();
 
         for (Sheet sheet : workbook) {
             for (Row row : sheet) {
-                if (tableY < margin + rowHeight) {
+                // If not enough space, move to a new page
+                if (yPosition - rowHeight < margin) {
                     contentStream.close();
                     page = new PDPage(PDRectangle.A4);
                     pdf.addPage(page);
                     contentStream = new PDPageContentStream(pdf, page);
                     contentStream.setFont(PDType1Font.HELVETICA, 10);
-                    tableY = yStart;
+                    yPosition = yStart;
                 }
 
                 float x = tableX;
                 for (int i = 0; i < numCols; i++) {
                     Cell cell = row.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-                    String value = getCellValue(cell, workbook.getCreationHelper().createFormulaEvaluator());
+                    String value = getCellValue(cell, evaluator);
 
                     // Draw cell border
                     contentStream.setStrokingColor(Color.BLACK);
-                    contentStream.addRect(x, tableY - rowHeight, colWidths[i], rowHeight);
+                    contentStream.addRect(x, yPosition - rowHeight, colWidths[i], rowHeight);
                     contentStream.stroke();
 
-                    // Write text
+                    // Write text with wrapping if necessary
                     contentStream.beginText();
-                    contentStream.newLineAtOffset(x + 2, tableY - 15);
-                    contentStream.showText(value);
+                    contentStream.newLineAtOffset(x + 2, yPosition - 15);
+                    contentStream.showText(value.length() > 15 ? value.substring(0, 15) + "..." : value); // Truncate if needed
                     contentStream.endText();
 
                     x += colWidths[i];
                 }
-                tableY -= rowHeight;
+                yPosition -= rowHeight;
             }
         }
         contentStream.close();
@@ -82,30 +68,3 @@ public class ExcelToPDF {
             imgStream.drawImage(pdImage, 50, 200, 400, 300);
             imgStream.close();
         }
-
-        pdf.save("ExcelToPDF.pdf");
-        pdf.close();
-        workbook.close();
-        fs.close();
-    }
-
-    private static String getCellValue(Cell cell, FormulaEvaluator evaluator) {
-        switch (cell.getCellType()) {
-            case STRING:
-                return cell.getStringCellValue();
-            case NUMERIC:
-                return String.valueOf(cell.getNumericCellValue());
-            case BOOLEAN:
-                return String.valueOf(cell.getBooleanCellValue());
-            case FORMULA:
-                return evaluator.evaluate(cell).formatAsString();
-            default:
-                return "";
-        }
-    }
-
-    private static byte[] getExcelByteArray() {
-        // Implement logic to get XLS file as byte[]
-        return new byte[0];
-    }
-}
