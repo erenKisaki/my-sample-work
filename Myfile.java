2ce8d52d11d758440b9a6ab378a35d152c2c79d8
erenKisaki <rakeshkisaki@gmail.com>
2025-02-28 18:07:57 +0530

    Update Myfile.java

--- Myfile.java
+++ Myfile.java
@@ -1,1 +1,20 @@
 
+    private static void drawWrappedText(PDPageContentStream contentStream, String text, float maxWidth) throws IOException {
+        String[] words = text.split(" ");
+        StringBuilder line = new StringBuilder();
+        float spaceWidth = 3;
+        float textWidth = 0;
+
+        for (String word : words) {
+            float wordWidth = PDType1Font.HELVETICA.getStringWidth(word) / 1000 * 10;
+            if (textWidth + wordWidth > maxWidth) {
+                contentStream.showText(line.toString());
+                contentStream.newLineAtOffset(0, -12);
+                line = new StringBuilder();
+                textWidth = 0;
+            }
+            line.append(word).append(" ");
+            textWidth += wordWidth + spaceWidth;
+        }
+        contentStream.showText(line.toString().trim());
+    }
