92bedae77201c920f476a62ffa7b739a6a55dd99
erenKisaki <rakeshkisaki@gmail.com>
2025-03-07 19:33:36 +0530

    Update ExcelToPDF.java

--- ExcelToPDF.java
+++ ExcelToPDF.java
@@ -1,100 +1,98 @@
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.pdmodel.font.*;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.poi.ss.usermodel.*;
+import java.io.*;
+import java.util.*;
 
+public class ExcelToPDFConverter {
+    private static final float MARGIN = 50;
+    private static final float ROW_HEIGHT = 20;
+    private static final float CELL_MARGIN = 5;
+    private static final PDFont FONT = PDType1Font.HELVETICA;
+    private static final int FONT_SIZE = 10;
+
+    public static void convertExcelToPDF(InputStream excelInput, OutputStream pdfOutput) throws IOException {
+        Workbook workbook = WorkbookFactory.create(excelInput);
+        PDDocument pdf = new PDDocument();
+        DataFormatter formatter = new DataFormatter();
+        
+        for (Sheet sheet : workbook) {
+            if (isSheetEmpty(sheet, formatter)) continue;
             PDPage page = new PDPage(PDRectangle.A4);
-            pdfDocument.addPage(page);
-            PDPageContentStream contentStream = new PDPageContentStream(pdfDocument, page);
-            contentStream.setFont(PDType1Font.HELVETICA, 10);
+            pdf.addPage(page);
+            PDPageContentStream contentStream = new PDPageContentStream(pdf, page);
+            contentStream.setFont(FONT, FONT_SIZE);
+            float yPosition = page.getMediaBox().getHeight() - MARGIN;
+            int numCols = getMaxColumns(sheet);
+            float tableWidth = PDRectangle.A4.getWidth() - 2 * MARGIN;
+            float colWidth = tableWidth / Math.max(1, numCols);
 
-            float margin = 50;
-            float yPosition = page.getMediaBox().getHeight() - margin;
-            float tableWidth = page.getMediaBox().getWidth() - 2 * margin;
-            float rowHeight = 20;
-            float cellMargin = 2;
-
-            Sheet sheet = workbook.getSheetAt(0);
-            List<List<String>> tableData = new ArrayList<>();
-            int numColumns = sheet.getRow(0).getPhysicalNumberOfCells();
-            float[] columnWidths = new float[numColumns];
-
-            // Read data and calculate column widths
             for (Row row : sheet) {
-                List<String> rowData = new ArrayList<>();
-                for (int i = 0; i < numColumns; i++) {
-                    Cell cell = row.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-                    String value = cell.toString();
-                    rowData.add(value);
-
-                    float textWidth = PDType1Font.HELVETICA.getStringWidth(value) / 1000 * 10; // Scale to font size
-                    columnWidths[i] = Math.max(columnWidths[i], textWidth + cellMargin * 2);
-                }
-                tableData.add(rowData);
-            }
-
-            // Normalize column widths to fit within table width
-            float totalWidth = 0;
-            for (float width : columnWidths) {
-                totalWidth += width;
-            }
-            float scaleFactor = tableWidth / totalWidth;
-            for (int i = 0; i < columnWidths.length; i++) {
-                columnWidths[i] *= scaleFactor;
-            }
-
-            // Draw table
-            for (List<String> row : tableData) {
-                if (yPosition - rowHeight < margin) {
-                    contentStream.close();
-                    page = new PDPage(PDRectangle.A4);
-                    pdfDocument.addPage(page);
-                    contentStream = new PDPageContentStream(pdfDocument, page);
-                    contentStream.setFont(PDType1Font.HELVETICA, 10);
-                    yPosition = page.getMediaBox().getHeight() - margin;
-                }
-
-                float xPosition = margin;
-                for (int i = 0; i < row.size(); i++) {
-                    String text = row.get(i);
-                    float colWidth = columnWidths[i];
-
-                    // Draw cell border
-                    contentStream.setLineWidth(1);
-                    contentStream.moveTo(xPosition, yPosition);
-                    contentStream.lineTo(xPosition + colWidth, yPosition);
-                    contentStream.lineTo(xPosition + colWidth, yPosition - rowHeight);
-                    contentStream.lineTo(xPosition, yPosition - rowHeight);
-                    contentStream.closeAndStroke();
-
-                    // Draw text
-                    contentStream.beginText();
-                    contentStream.newLineAtOffset(xPosition + cellMargin, yPosition - 15);
-                    contentStream.showText(text);
-                    contentStream.endText();
-
+                if (isRowEmpty(row, formatter)) continue;
+                float xPosition = MARGIN;
+                
+                for (Cell cell : row) {
+                    String cellValue = formatter.formatCellValue(cell).replaceAll("[\t\n\r]+", " ");
+                    if (xPosition + colWidth > page.getMediaBox().getWidth() - MARGIN) {
+                        contentStream.close();
+                        page = new PDPage(PDRectangle.A4);
+                        pdf.addPage(page);
+                        contentStream = new PDPageContentStream(pdf, page);
+                        contentStream.setFont(FONT, FONT_SIZE);
+                        yPosition = page.getMediaBox().getHeight() - MARGIN;
+                        xPosition = MARGIN;
+                    }
+                    drawCell(contentStream, xPosition, yPosition, colWidth, ROW_HEIGHT, cellValue);
                     xPosition += colWidth;
                 }
-                yPosition -= rowHeight;
+                yPosition -= ROW_HEIGHT;
+                if (yPosition < MARGIN) {
+                    contentStream.close();
+                    page = new PDPage(PDRectangle.A4);
+                    pdf.addPage(page);
+                    contentStream = new PDPageContentStream(pdf, page);
+                    contentStream.setFont(FONT, FONT_SIZE);
+                    yPosition = page.getMediaBox().getHeight() - MARGIN;
+                }
             }
-        contentStream.close();
-        
-        // Process images below the table
-        float imgX = margin;
-        float imgY = yPosition - 100;
-        for (HSSFPictureData picture : workbook.getAllPictures()) {
-            if (imgY < margin + 100) {
-                page = new PDPage(PDRectangle.A4);
-                pdf.addPage(page);
-                imgX = margin;
-                imgY = yStart - 100;
-            }
-
-            byte[] imageBytes = picture.getData();
-            BufferedImage bImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
-            File imageFile = File.createTempFile("excel_image", ".png");
-            ImageIO.write(bImage, "png", imageFile);
-            PDImageXObject pdImage = PDImageXObject.createFromFile(imageFile.getAbsolutePath(), pdf);
-
-            PDPageContentStream imgStream = new PDPageContentStream(pdf, page, PDPageContentStream.AppendMode.APPEND, true);
-            imgStream.drawImage(pdImage, imgX, imgY, 200, 150);
-            imgStream.close();
-
-            imgY -= 160;
+            contentStream.close();
         }
+        pdf.save(pdfOutput);
+        pdf.close();
+        workbook.close();
+    }
+
+    private static boolean isSheetEmpty(Sheet sheet, DataFormatter formatter) {
+        for (Row row : sheet) {
+            if (!isRowEmpty(row, formatter)) return false;
+        }
+        return true;
+    }
+
+    private static boolean isRowEmpty(Row row, DataFormatter formatter) {
+        for (Cell cell : row) {
+            if (!formatter.formatCellValue(cell).trim().isEmpty()) return false;
+        }
+        return true;
+    }
+
+    private static int getMaxColumns(Sheet sheet) {
+        int maxCols = 0;
+        for (Row row : sheet) {
+            if (row.getLastCellNum() > maxCols) {
+                maxCols = row.getLastCellNum();
+            }
+        }
+        return maxCols;
+    }
+
+    private static void drawCell(PDPageContentStream contentStream, float x, float y, float width, float height, String text) throws IOException {
+        contentStream.beginText();
+        contentStream.newLineAtOffset(x + CELL_MARGIN, y - height + CELL_MARGIN);
+        contentStream.showText(text);
+        contentStream.endText();
+        contentStream.addRect(x, y - height, width, height);
+        contentStream.stroke();
+    }
+}
