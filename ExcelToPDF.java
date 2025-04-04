2b5e3c19a453d3403f41f01c26acbfd2678bf6f7
erenKisaki <rakeshkisaki@gmail.com>
2025-04-04 17:17:15 +0530

    Update ExcelToPDF.java

--- ExcelToPDF.java
+++ ExcelToPDF.java
@@ -1,6 +1,11 @@
-@When("the user selects {string} from the Application dropdown")
-public void theUserSelectsFromTheApplicationDropdown(String application) {
-    WebElement applicationDropdown = driver.findElement(By.name("application")); // Updated based on UI reference
-    Select dropdown = new Select(applicationDropdown);
-    dropdown.selectByVisibleText(application);
-}
+    def extract_trans_code(self, response):
+        """
+        Extract transCode from the JSON response.
+        """
+        try:
+            data = response.json()
+            trans_code = data.get("transCode")
+            return trans_code
+        except json.JSONDecodeError as e:
+            logger.error("Error parsing JSON response: %s", str(e))
+            raise Exception("Invalid JSON response") from e
