c73747b323b1732d23c4481ac374823f95d3ade0
erenKisaki <rakeshkisaki@gmail.com>
2025-04-07 19:48:30 +0530

    Update ExcelToPDF.java

--- ExcelToPDF.java
+++ ExcelToPDF.java
@@ -1,11 +1,8 @@
-    def extract_trans_code(self, response):
-        """
-        Extract transCode from the JSON response.
-        """
-        try:
-            data = response.json()
-            trans_code = data.get("transCode")
-            return trans_code
-        except json.JSONDecodeError as e:
-            logger.error("Error parsing JSON response: %s", str(e))
-            raise Exception("Invalid JSON response") from e
+while (true) {
+    s3ObjectSummaries.addAll(objectListing.getObjectSummaries());
+    if (objectListing.isTruncated()) {
+        objectListing = s3Client.listNextBatchOfObjects(objectListing);
+    } else {
+        break;
+    }
+}
