82989eceb0fdf784d5bdd1ae0f4671fc000dcb5b
erenKisaki <rakeshkisaki@gmail.com>
2025-04-07 20:13:07 +0530

    Update ExcelToPDF.java

--- ExcelToPDF.java
+++ ExcelToPDF.java
@@ -1,8 +1,15 @@
-while (true) {
-    s3ObjectSummaries.addAll(objectListing.getObjectSummaries());
-    if (objectListing.isTruncated()) {
-        objectListing = s3Client.listNextBatchOfObjects(objectListing);
-    } else {
-        break;
+ListObjectsV2Request req = new ListObjectsV2Request().withBucketName(s3DataDobiBucket);
+ListObjectsV2Result result;
+
+do {
+    result = s3Client.listObjectsV2(req);
+
+    for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
+        keys.add(objectSummary.getKey());
+        LOGGER.logInfo("All objects retrieved: " + objectSummary.getKey());
     }
-}
+
+    // If there's more to retrieve, set the continuation token
+    req.setContinuationToken(result.getNextContinuationToken());
+
+} while (result.isTruncated());
