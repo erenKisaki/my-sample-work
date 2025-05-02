074d280c5950c977cb004ea29f46837c9d3c5238
erenKisaki <rakeshkisaki@gmail.com>
2025-05-02 19:15:35 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,41 +1,33 @@
-public static byte[] convertAFPtoPDF(final byte[] afpFile) throws HdpException {
-    LOG.info("Inside the method of convertAFPtoPDF");
-    try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
-        List<byte[]> tiffImgList = AFPConverter.processAfpFile(afpFile);
-        try (PDDocument document = new PDDocument()) {
-            tiffImgList.stream().forEach(tiffImg -> {
-                try {
-                    InputStream is = new ByteArrayInputStream(tiffImg);
-                    ImageInputStream iis = ImageIO.createImageInputStream(is);
-                    Iterator<ImageReader> iterator = ImageIO.getImageReaders(iis);
-                    ImageReader reader = iterator.next();
-                    reader.setInput(iis, false, true);
-                    int nbPages = reader.getNumImages(true);
-
-                    for (int p = 0; p < nbPages; p++) {
-                        BufferedImage bufferedImage = reader.read(p, reader.getDefaultReadParam());
-                        
-                        // Set the PDF page size same as the image
-                        float width = bufferedImage.getWidth();
-                        float height = bufferedImage.getHeight();
-                        PDPage page = new PDPage(new PDRectangle(width, height));
-                        document.addPage(page);
-
-                        PDImageXObject imageXObject = LosslessFactory.createFromImage(document, bufferedImage);
-                        try (PDPageContentStream content = new PDPageContentStream(document, page)) {
-                            content.drawImage(imageXObject, 0, 0, width, height);
-                        }
-                    }
-                } catch (Exception e) {
-                    LOG.error("ERROR CONVERTING TIF image to PDF using PDFBOX - Message=[{}], trying ITEXT ", e.getMessage());
-                    throw new HdpException(new Message(e.getMessage(), CommonErrors.HDP_ERROR.getCode(), MessageType.ERROR));
+    // Handle contribution limits
+if (RestrictionConstants.ACCOUNTTYPELISTS.contains(accountTypeCode)) {
+    ContributionLimitExceededResponse limitResponse = new ContributionLimitExceededResponse();
+    ResponseEntity<ET3Response> et3Entity = et3Service.validateDepositVBALockbox(clientPoid, requestBody.getAmount(), requestBody.getAccount());
+    List<VOMRuleDetails> voMRulDetails = new ArrayList<>();
+    if (et3Entity.getStatusCode() == HttpStatus.OK) {
+        ET3Response et3Response = et3Entity.getBody();
+        boolean isDc2ServiceCall = true;
+        if (null != et3Response && et3Response.getProcessedTradeTransaction() != null
+                && et3Response.getProcessedTradeTransaction().getProcessedTradeTransactionDetail() != null
+                && et3Response.getProcessedTradeTransaction().getProcessedTradeTransactionDetail().getTriggeredTradingRules() != null
+                && et3Response.getProcessedTradeTransaction().getProcessedTradeTransactionDetail().getTriggeredTradingRules().size() > 0) {
+            List<ET3Response.TriggeredTradingRule> triggeredTradingRules = et3Response.getProcessedTradeTransaction().getProcessedTradeTransactionDetail().getTriggeredTradingRules();
+            for (ET3Response.TriggeredTradingRule triggeredTradingRule : triggeredTradingRules) {
+                VOMRuleDetails voMRulDetail = new VOMRuleDetails();
+                voMRulDetail.setVoMRulSeverity(getRuleSeverity(triggeredTradingRule.getRuleAction()));
+                voMRulDetail.setVoMRulId(triggeredTradingRule.getRuleId());
+                voMRulDetails.add(voMRulDetail);
+                
+                if(triggeredTradingRule.getRuleAction() == RuleAction.WARNING && triggeredTradingRule.getRuleAction() == RuleAction.ERROR) {
+                    isDc2ServiceCall = false;
                 }
-            });
-            document.save(os);
+            }
+            if(isDc2ServiceCall) {
+                RetirementContributionSummaryResponse dc2Response = dc2ContributionService.getContributionLimits(clientPoid);
+                limitResponse = dc2Mapper.mapToContributionLimitResponse(dc2Response, requestBody.getAmount(), requestBody.getTransactionCode(), accountTypeCode, accountId);
+            }
+            limitResponse.setVoMRulDetails(voMRulDetails);
+            return ResponseEntity.status(HttpStatus.CREATED).body(limitResponse);
         }
-        LOG.info("Successfully converted from [AFP] Non Annotated Image file to PDF");
-        return os.toByteArray();
-    } catch (Exception e) {
-        throw new HdpException(new Message(e.getMessage(), CommonErrors.HDP_ERROR.getCode(), MessageType.ERROR));
+    } else {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ET3 service call failed");
     }
-}
