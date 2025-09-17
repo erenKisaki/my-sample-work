49df642f9f31f4b12223540bf7c2b8e04132e4c0
erenKisaki <rakeshkisaki@gmail.com>
2025-09-17 17:08:06 +0530

    Update SSL.java

--- SSL.java
+++ SSL.java
@@ -1,17 +1,11 @@
-import java.security.SecureRandom;
-import java.util.stream.Collectors;
-
-public class RandomAlphaStream {
+    public static String generateRandomString(boolean onlyAlphabets, int length) {
+        String source = onlyAlphabets ? ALPHABETS : ALPHANUMERIC;
+        return RANDOM.ints(length, 0, source.length())
+                     .mapToObj(source::charAt)
+                     .map(String::valueOf)
+                     .collect(Collectors.joining());
+    }
 
     private static final String ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final SecureRandom RANDOM = new SecureRandom();
-
-    public static void main(String[] args) {
-        String randomString = RANDOM.ints(11, 0, ALPHABETS.length())
-                .mapToObj(ALPHABETS::charAt)
-                .map(String::valueOf)
-                .collect(Collectors.joining());
-
-        System.out.println("Random String: " + randomString);
-    }
-}
