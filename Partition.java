d8ddf215ef9840b8ca55b48578876fb9ec6cd382
erenKisaki <rakeshkisaki@gmail.com>
2025-12-24 21:50:58 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,24 +1,49 @@
-String otp = null;
+			
+		DateTimeFormatter ORACLE_FORMAT =
+        DateTimeFormatter.ofPattern("dd-MMM-yy hh.mm.ss.nnnnnnnnn a", Locale.ENGLISH);
 
-ExecutorService executor = Executors.newSingleThreadExecutor();
-Future<String> future = executor.submit(() -> 
-    JOptionPane.showInputDialog("Enter the OTP received on your email or phone (You have 5 minutes):")
-);
+        LocalDateTime lastExecution =
+            LocalDateTime.parse(lastExecutionStr, ORACLE_FORMAT);
 
-try {
-    otp = future.get(5, TimeUnit.MINUTES); // Wait max 5 minutes
-    if (otp == null || otp.isEmpty()) {
-        System.out.println("No OTP entered. Exiting...");
-        return;
-    }
-    System.out.println("OTP entered: " + otp);
-} catch (TimeoutException e) {
-    System.out.println("OTP entry timed out!");
-    future.cancel(true);
-    return;
-} catch (Exception e) {
-    e.printStackTrace();
-    return;
-} finally {
-    executor.shutdownNow();
-}
+        LocalDate candidateDate = lastExecution.toLocalDate().plusDays(1);
+
+        while (candidateDate.getDayOfWeek() == DayOfWeek.SATURDAY ||
+               candidateDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            candidateDate = candidateDate.plusDays(1);
+        }
+
+        List<LocalDateTime> matches = new ArrayList<>();
+
+        for (ScheduleRule rule : rules) {
+
+            if (!rule.active) continue;
+
+            boolean match = false;
+
+            if (rule.dateOfMonth != null &&
+                    rule.dateOfMonth == candidateDate.getDayOfMonth())
+                match = true;
+
+            if (rule.dayOfWeek != null &&
+                    rule.dayOfWeek == candidateDate.getDayOfWeek())
+                match = true;
+
+            if (match) {
+                matches.add(LocalDateTime.of(candidateDate, rule.time));
+            }
+        }
+
+        LocalDateTime nextExecution;
+
+        if (!matches.isEmpty()) {
+            nextExecution = matches.stream()
+                    .min(LocalDateTime::compareTo)
+                    .get();
+        } else {
+            nextExecution = LocalDateTime.of(
+                    candidateDate,
+                    lastExecution.toLocalTime()
+            );
+        }
+
+        return nextExecution.format(OUTPUT_FORMAT);
