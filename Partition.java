4471b4f62c5b5e6ea80dc9908896f51b6a0b2b0f
erenKisaki <rakeshkisaki@gmail.com>
2026-01-06 00:50:04 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,37 +1,7 @@
-import java.util.Random;
+ZoneId estZone = ZoneId.of("America/New_York");
 
-public class KeyGenerator {
+Date scheduledDate = paymentProcessing.getScheduledDate();
 
-    private static final Random random = new Random();
-
-    public static String randomPrimaryKey() {
-        String chars = "12345";
-        int length = 5;
-        StringBuilder result = new StringBuilder();
-
-        for (int i = length; i > 0; i--) {
-            int index = random.nextInt(chars.length());
-            result.append(chars.charAt(index));
-        }
-
-        return result.toString();
-    }
-
-    public static String randomSecondaryKey() {
-        String chars = "123";
-        int length = 3;
-        StringBuilder result = new StringBuilder();
-
-        for (int i = length; i > 0; i--) {
-            int index = random.nextInt(chars.length());
-            result.append(chars.charAt(index));
-        }
-
-        return result.toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Primary Key   : " + randomPrimaryKey());
-        System.out.println("Secondary Key : " + randomSecondaryKey());
-    }
-}
+LocalDateTime currentDateTime= scheduledDate.toInstant()
+        .atZone(estZone)
+        .toLocalDateTime();
