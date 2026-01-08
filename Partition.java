d56fdefe3b14e5944a950ac324c20bf397781e93
erenKisaki <rakeshkisaki@gmail.com>
2026-01-08 20:50:44 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,5 +1,39 @@
-ZoneId estZone = ZoneId.of("America/New_York");
+public LocalDateTime getEligibleNextRun(LocalDateTime currentDate,
+                                        LocalDateTime scheduleDate,
+                                        PaymentProcessing paymentProcessing,
+                                        long resultTime) {
 
-var currentDateTime= scheduledDate.toInstant()
-        .atZone(estZone)
-        .toLocalDateTime();
+    boolean isEligibleRunFound = false;
+    LocalDateTime runDate = null;
+
+    // Track first attempt and second attempt
+    int attempts = 0;
+    LocalDateTime fallbackNextDay = null;
+
+    while (!isEligibleRunFound) {
+
+        runDate = getNextRun(runDate, currentDate, resultTime);
+
+        // Save "plus one day" as fallback
+        if (attempts == 0) {
+            fallbackNextDay = runDate;
+        }
+
+        if (isAnyExceptionRuleMatches(runDate,
+                                      scheduleDate,
+                                      paymentProcessing.getErrorCode())) {
+            isEligibleRunFound = true;
+        }
+
+        attempts++;
+
+        // After checking current date & plus one day → apply default
+        if (!isEligibleRunFound && attempts >= 2) {
+            return fallbackNextDay;
+        }
+    }
+
+    return runDate;
+}
+
+
