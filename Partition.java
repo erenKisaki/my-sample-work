9f5e336bb90c774a8d808552fc61960ac7e7d549
erenKisaki <rakeshkisaki@gmail.com>
2026-01-10 02:08:25 +0530

    Mock BatchPaymentWorkListUtil for testing

--- Partition.java
+++ Partition.java
@@ -1,4 +1,7 @@
-if (earliestExceptionDate != null) {
-        runDate = earliestExceptionDate;
-        exceptionMatched = true;
-}
+List<BatchRetryCustomException> mockExceptions = new ArrayList<>();
+
+MockedStatic<BatchPaymentWorkListUtil> mocked =
+                 mockStatic(BatchPaymentWorkListUtil.class);
+
+ mocked.when(BatchPaymentWorkListUtil::getBatchRetryCustomExceptionList)
+              .thenReturn(mockExceptions);
