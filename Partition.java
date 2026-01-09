d71d0c9551b32eac9767a92c2ad50de3de379bf7
erenKisaki <rakeshkisaki@gmail.com>
2026-01-10 02:27:03 +0530

    Refactor Partition.java for timezone handling
    Removed PowerMock setup and added timezone handling.

--- Partition.java
+++ Partition.java
@@ -1,10 +1,2 @@
-
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(BatchPaymentWorkListUtil.class);
-
-List<BatchRetryCustomException> mockExceptions = new ArrayList<>();
-
-PowerMockito.mockStatic(BatchPaymentWorkListUtil.class);
-
-PowerMockito.when(BatchPaymentWorkListUtil.getBatchRetryCustomExceptionList())
-            .thenReturn(mockExceptions);
+ZoneId estZone = ZoneId.of("America/New_York");
+LocalDateTime now = LocalDateTime.now(estZone);
