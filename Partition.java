5e94c0faee83c8e67a3eac830dd0ec4d640c2949
erenKisaki <rakeshkisaki@gmail.com>
2026-01-13 01:52:18 +0530

    Add locators for login page elements

--- Partition.java
+++ Partition.java
@@ -1,1 +1,7 @@
+By USERNAME     = By.xpath("//input[@id='main_UID']");
+By PASSWORD     = By.xpath("//input[@id='main_PWD']");
+By REMEMBER_ME  = By.xpath("//div[@id='main_divRemMe']//input[@type='checkbox']");
+By SIGN_IN_BTN  = By.xpath("//input[@id='main_btnSubmit']");
+By FORGOT_USER  = By.xpath("//a[normalize-space()='Forgot Username?']");
+By FORGOT_PWD   = By.xpath("//a[normalize-space()='Forgot Password?']");
 
