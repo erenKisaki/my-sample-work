e6c03030dc56db4875411b10ef567eb786b70b45
erenKisaki <rakeshkisaki@gmail.com>
2026-01-12 16:27:40 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,1 +1,4 @@
+//h1[normalize-space()='User Locked or Disabled']
+//a[@class='alink' and contains(@href,'forgot-password')]
+//p[contains(normalize-space(),'Having trouble resetting your password')]
 
