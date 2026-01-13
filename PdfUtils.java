1270ac874bbf22f49ef3424309e7e18276b0220d
erenKisaki <rakeshkisaki@gmail.com>
2026-01-13 21:06:04 +0530

    Update PdfUtils.java

--- PdfUtils.java
+++ PdfUtils.java
@@ -1,41 +1,63 @@
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.common.PDStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+@ExtendWith(MockitoExtension.class)
+class PaymentProcessingHistoryDaoImplTest {
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+    @InjectMocks
+    private PaymentProcessingHistoryDaoImpl dao;
 
-public class PdfUtils {
-    public static byte[] createPdfWithEmbeddedFile(byte[] fileBytes) throws IOException {
-        try (PDDocument document = new PDDocument()) {
-            PDPage page = new PDPage();
-            document.addPage(page);
+    @Mock
+    private PaymentDatabaseConnector paymentDatabaseConnector;
 
-            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                contentStream.beginText();
-                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-                contentStream.newLineAtOffset(100, 700);
-                contentStream.showText("This PDF contains an embedded file.");
-                contentStream.endText();
-            }
+    @Mock
+    private JdbcTemplate jdbcTemplate;
 
-            PDStream pdStream = new PDStream(document, new ByteArrayInputStream(fileBytes));
-            pdStream.addCompression();
-
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            document.save(baos);
-
-            return baos.toByteArray();
-        }
+    @BeforeEach
+    void setup() {
+        when(paymentDatabaseConnector.getJdbcTemplate()).thenReturn(jdbcTemplate);
     }
 
-    public static void writeBytesToPdf(byte[] pdfBytes, String filePath) throws IOException {
-        try (FileOutputStream fos = new FileOutputStream(filePath)) {
-            fos.write(pdfBytes);
-        }
+    @Test
+    void add_ShouldInsertSuccessfully() {
+        PaymentProcessingHistory history = new PaymentProcessingHistory();
+
+        PaymentProcessingHistoryDaoImpl spyDao = spy(dao);
+        doReturn(100L).when(spyDao).getNextSeqNumber(anyString());
+        doReturn(1).when(jdbcTemplate).update(anyString(), any());
+
+        Long result = spyDao.add(history);
+
+        assertNotNull(result);
+        assertEquals(100L, result);
+    }
+
+    @Test
+    void add_ShouldThrowException_WhenSequenceNull() {
+        PaymentProcessingHistory history = new PaymentProcessingHistory();
+
+        PaymentProcessingHistoryDaoImpl spyDao = spy(dao);
+        doReturn(null).when(spyDao).getNextSeqNumber(anyString());
+
+        assertThrows(RuntimeException.class, () -> spyDao.add(history));
+    }
+
+    @Test
+    void insert_ShouldReturnSequence_WhenInsertSuccess() {
+        PaymentProcessingHistory history = new PaymentProcessingHistory();
+
+        when(jdbcTemplate.update(anyString(), any())).thenReturn(1);
+
+        Long result = dao.insert(history, 10L);
+
+        assertEquals(10L, result);
+    }
+
+    @Test
+    void insert_ShouldReturnNull_WhenInsertFails() {
+        PaymentProcessingHistory history = new PaymentProcessingHistory();
+
+        when(jdbcTemplate.update(anyString(), any())).thenReturn(0);
+
+        Long result = dao.insert(history, 10L);
+
+        assertNull(result);
     }
 }
