a6014cecd69212fab26ce0a410b1489b6f2de199
erenKisaki <rakeshkisaki@gmail.com>
2026-01-16 19:03:49 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,4 +1,29 @@
-List<BatchReauthCustomException> sortedList =
-        exceptionList.stream()
-                .sorted(Comparator.comparingLong(BatchBaseException::getTime))
-                .collect(Collectors.toList());
+private boolean isAnyExceptionRuleMatches(LocalDateTime runDate, String errorCode) {
+
+    List<BatchReauthCustomException> exceptionList =
+            BatchPaymentWorkListUtil.getBatchReauthCustomExceptionList();
+
+    // 🔹 Sort exception rules by time (ascending)
+    exceptionList = exceptionList.stream()
+            .sorted(Comparator.comparingLong(BatchReauthCustomException::getTime))
+            .collect(Collectors.toList());
+
+    for (BatchReauthCustomException exceptionRule : exceptionList) {
+
+        if (isExceptionDayMatches(exceptionRule, runDate)
+                || isExceptionDayOfTheMonthMatches(exceptionRule, runDate)) {
+
+            if (StringUtils.isNotEmpty(exceptionRule.getIncludeError())
+                    && StringUtils.isNotEmpty(errorCode)
+                    && exceptionRule.getIncludeError().contains(errorCode)
+                    && runDate.toLocalTime()
+                               .equals(convertToLocalTime(exceptionRule.getTime()))) {
+                return true;
+            }
+        }
+        else if (!isErrorCodeMatches(exceptionRule.getExcludeError(), errorCode)) {
+            return true;
+        }
+    }
+    return false;
+}
