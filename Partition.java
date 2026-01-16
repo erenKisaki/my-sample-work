e93331591658d9f14d24c81afe623063a234f244
erenKisaki <rakeshkisaki@gmail.com>
2026-01-17 00:27:33 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,29 +1,5 @@
-private boolean isAnyExceptionRuleMatches(LocalDateTime runDate, String errorCode) {
+ boolean hasApplicableRule = false;
 
-    List<BatchReauthCustomException> exceptionList =
-            BatchPaymentWorkListUtil.getBatchReauthCustomExceptionList();
+hasApplicableRule = true;
 
-    // 🔹 Sort exception rules by time (ascending)
-    exceptionList = exceptionList.stream()
-            .sorted(Comparator.comparingLong(BatchReauthCustomException::getTime))
-            .collect(Collectors.toList());
-
-    for (BatchReauthCustomException exceptionRule : exceptionList) {
-
-        if (isExceptionDayMatches(exceptionRule, runDate)
-                || isExceptionDayOfTheMonthMatches(exceptionRule, runDate)) {
-
-            if (StringUtils.isNotEmpty(exceptionRule.getIncludeError())
-                    && StringUtils.isNotEmpty(errorCode)
-                    && exceptionRule.getIncludeError().contains(errorCode)
-                    && runDate.toLocalTime()
-                               .equals(convertToLocalTime(exceptionRule.getTime()))) {
-                return true;
-            }
-        }
-        else if (!isErrorCodeMatches(exceptionRule.getExcludeError(), errorCode)) {
-            return true;
-        }
-    }
-    return false;
-}
+return hasApplicableRule;
