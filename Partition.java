b013f18330f2191621613af3385b0af5dca4c8c5
erenKisaki <rakeshkisaki@gmail.com>
2026-01-16 18:54:11 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,24 +1,4 @@
-@Test
-public void testNextRunForAllDays_EvenTimeOnly() {
-
-    assertTrue(LocalDateTime.of(2026, 1, 13, 18, 0)
-            .equals(scheduleDateAdapter.getNextRun(LocalDateTime.of(2026, 1, 12, 10, 0), 2))); // Mon → Tue
-
-    assertTrue(LocalDateTime.of(2026, 1, 14, 18, 0)
-            .equals(scheduleDateAdapter.getNextRun(LocalDateTime.of(2026, 1, 13, 10, 0), 2))); // Tue → Wed
-
-    assertTrue(LocalDateTime.of(2026, 1, 15, 18, 0)
-            .equals(scheduleDateAdapter.getNextRun(LocalDateTime.of(2026, 1, 14, 10, 0), 2))); // Wed → Thu
-
-    assertTrue(LocalDateTime.of(2026, 1, 16, 18, 0)
-            .equals(scheduleDateAdapter.getNextRun(LocalDateTime.of(2026, 1, 15, 10, 0), 2))); // Thu → Fri
-
-    assertTrue(LocalDateTime.of(2026, 1, 19, 18, 0)
-            .equals(scheduleDateAdapter.getNextRun(LocalDateTime.of(2026, 1, 16, 10, 0), 2))); // Fri → Mon
-
-    assertTrue(LocalDateTime.of(2026, 1, 19, 18, 0)
-            .equals(scheduleDateAdapter.getNextRun(LocalDateTime.of(2026, 1, 17, 10, 0), 2))); // Sat → Mon
-
-    assertTrue(LocalDateTime.of(2026, 1, 19, 18, 0)
-            .equals(scheduleDateAdapter.getNextRun(LocalDateTime.of(2026, 1, 18, 10, 0), 2))); // Sun → Mon
-}
+List<BatchReauthCustomException> sortedList =
+        exceptionList.stream()
+                .sorted(Comparator.comparingLong(BatchBaseException::getTime))
+                .collect(Collectors.toList());
