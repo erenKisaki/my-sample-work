fe04181517b2955f9a79688724a24fb8bc08e5df
erenKisaki <rakeshkisaki@gmail.com>
2026-01-23 22:57:22 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -2,3 +2,9 @@ when(jdbcTemplate.update(
         anyString(),
         ArgumentMatchers.<Object[]>any()
 )).thenReturn(1);
+
+
+verify(jdbcTemplate, times(1)).update(
+        anyString(),
+        ArgumentMatchers.<Object[]>any()
+);
