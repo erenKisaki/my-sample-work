6c639d83ed51899b275fc4baa38596c30ddc8d83
erenKisaki <rakeshkisaki@gmail.com>
2026-01-23 22:57:48 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -8,3 +8,18 @@ verify(jdbcTemplate, times(1)).update(
         anyString(),
         ArgumentMatchers.<Object[]>any()
 );
+
+@Test
+void shouldKeepAllWhenAllMatch() {
+    BatchPaymentWorkListUtil.batchReauthCustomExceptionList = Arrays.asList(
+        new BatchReauthCustomException(5L),
+        new BatchReauthCustomException(5L)
+    );
+
+    BatchPaymentWorkListUtil.updateBatchReauthCustomExceptionListByReauthRule(5L);
+
+    assertEquals(
+        2,
+        BatchPaymentWorkListUtil.batchReauthCustomExceptionList.size()
+    );
+}
