1ae12e55872d29ae9ca701b0b2e2a331ac4e914c
erenKisaki <rakeshkisaki@gmail.com>
2026-01-27 23:47:23 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,1 +1,36 @@
-"^\"\\d{18}\"$"
+
+		
+private BillingInfo validateTokenConfiguration(BillingInfo billingInfo) {
+
+    var tokenRegex =
+            configuration.getString("token.configuration.regex");
+
+    String token = AuditCollectorRO.get()
+            .getPaymentServiceTransactions()
+            .getToken();
+
+    if (StringUtils.isNotBlank(tokenRegex)
+            && StringUtils.isNotBlank(token)
+            && token.matches(tokenRegex)) {
+
+    //return createEnhancedBillingInfo(billingInfo.getBillingArrangementId(), billingInfo); // Remove this line and add the below code in this method
+		if (StringUtils.isNotBlank(billingInfo.getMarket())) {
+
+            var xbuckMarkets =
+                    configuration.getStringList("xbuck.bypass.ass.enabled.markets");
+
+            if (CollectionUtils.isNotEmpty(xbuckMarkets)
+                    && StringUtils.isNotBlank(billingInfo.getMarket())
+                    && xbuckMarkets.contains(billingInfo.getMarket())) {
+
+                return createEnhancedBillingInfo(
+                        billingInfo.getBillingArrangementId(),
+                        billingInfo
+                );
+            }
+		}
+        
+    }
+
+    return null;
+}
