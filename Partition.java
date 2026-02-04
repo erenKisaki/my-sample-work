f5199440416aa4daa4285694244203fac5068f23
erenKisaki <rakeshkisaki@gmail.com>
2026-02-04 22:46:24 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,96 +1,58 @@
-public Optional<LevelProcessingData> getLevelProcessingData(
-        LevelProcessingRequest levelProcessingRequest) {
 
-    Optional<LevelProcessingData> levelProcessingOptionalData = Optional.empty();
+    private void validateBankRoutingNumber(String routingNumber) {
 
-    var levelEligibility =
-            levelEligibilityAdapter.getLevelEligibility(levelProcessingRequest);
-
-    BigDecimal stateTax;
-
-    if (levelEligibility.isPresent()) {
-        LOGGER.debug("Not a Level Processing request. Follow standard processing.");
-        return levelProcessingOptionalData;
-    }
-
-    var level = levelEligibility.get();
-    var levelProcessingData = new LevelProcessingData();
-    levelProcessingData.setPurchasingLevel(level.toString());
-    levelProcessingData.setChannel(levelProcessingRequest.getChannel());
-
-    var levelProcessingAMSData = levelProcessingData.getLevelProcessingAMSData();
-
-    if (isLevel2(level)) {
-        stateTax = getStateTaxForLevel2(
-                levelProcessingRequest, levelProcessingData, levelProcessingAMSData);
-        handleLevel2(stateTax, level, levelProcessingAMSData);
-    } else if (isLevel3(level)) {
-        handleLevel3(
-                levelProcessingRequest, levelProcessingData, levelProcessingAMSData);
-    }
-
-    setupLevelProcessTxnObject(level, levelProcessingAMSData);
-    return Optional.of(levelProcessingData);
-}
-
-private void handleLevel2(BigDecimal stateTax,
-                          Long level,
-                          LevelProcessingAMSData levelProcessingAMSData) {
-
-    if (Objects.isNull(stateTax)) {
-        if (levelProcessingAMSData.getDowngradeReason().isPresent()) {
-            levelProcessingAMSData.setDowngradeReason(
-                    Optional.of("StateTax not available in AccountManagementService."));
+        // Original outer condition preserved
+        if (StringUtils.isNotBlank(routingNumber)) {
+            validateNumeric(routingNumber);
+            validateLength(routingNumber);
+            validateFirstTwoDigits(routingNumber);
+            validateCheckDigit(routingNumber);
         }
-        return;
     }
 
-    if (stateTax.compareTo(BigDecimal.ZERO) > 0) {
-        levelProcessingAMSData.setLevelEnabled(true);
-        levelProcessingAMSData.setStateTaxAmount(stateTax);
-    } else {
-        levelProcessingAMSData.setDowngradeReason(
-                Optional.of("Negative tax amount in AMS. Downgraded to standard"));
+    // -------------------- Helper Methods --------------------
+
+    private void validateNumeric(String routingNumber) {
+        if (!StringUtils.isNumeric(routingNumber)) {
+            throw new RequestValidationError(
+                    ErrorCodes.PAYMENT_1027.getCode());
+        }
     }
-}
-private void handleLevel3(LevelProcessingRequest levelProcessingRequest,
-                          LevelProcessingData levelProcessingData,
-                          LevelProcessingAMSData levelProcessingAMSData) {
 
-    levelProcessingData.setStateTaxAmount(BigDecimal.ZERO);
-
-    Address address =
-            amsAdapter.getAddressFromQueryBillingArrangement(
-                    levelProcessingRequest.getBillingArrangementId(),
-                    Boolean.TRUE.equals(levelProcessingRequest.getLevelProcessing()),
-                    levelProcessingAMSData);
-
-    if (isValidAddress(address)) {
-        levelProcessingData.setLevelEnabled(true);
-        levelProcessingData.setState(address.getState());
-        levelProcessingData.setZipCode(address.getZipCode());
-        levelProcessingData.setPsCode(address.getPsCode());
-
-        var channel = channelLevelPropertiesDao
-                .getChannelLevelProperties(levelProcessingRequest.getChannel());
-        levelProcessingData.setCommodityCode(channel.getCommodityCode());
-    } else {
-        levelProcessingAMSData.setDowngradeReason(
-                Optional.of("Call to AccountManagementService failed or address or PsCode not returned in AMS"));
+    private void validateLength(String routingNumber) {
+        // bank routing number numeric, 9-digit
+        if (routingNumber.length() != 9) {
+            throw new RequestValidationError(
+                    ErrorCodes.PAYMENT_1028.getCode());
+        }
     }
-}
 
-private boolean isLevel2(Long level) {
-    return Long.valueOf(PaymentServiceConstants.LEVEL_2).equals(level);
-}
+    private void validateFirstTwoDigits(String routingNumber) {
+        // routing number first two digits should be valid
+        int firstTwoDigits = Integer.parseInt(routingNumber.substring(0, 2));
 
-private boolean isLevel3(Long level) {
-    return Long.valueOf(PaymentServiceConstants.LEVEL_3).equals(level);
-}
+        if ((firstTwoDigits <= 0)
+                || (firstTwoDigits >= 6 && firstTwoDigits <= 12)
+                || (firstTwoDigits >= 21 && firstTwoDigits <= 32)
+                || (firstTwoDigits >= 61 && firstTwoDigits <= 72)) {
 
-private boolean isValidAddress(Address address) {
-    return address != null
-            && StringUtils.isNotEmpty(address.getState())
-            && StringUtils.isNotEmpty(address.getZipCode())
-            && StringUtils.isNotEmpty(address.getPsCode());
-}
+            throw new RequestValidationError(
+                    ErrorCodes.PAYMENT_1250.getCode());
+        }
+    }
+
+    private void validateCheckDigit(String routingNumber) {
+        // Routing number check digit validation
+        int checkValue = 0;
+
+        for (int index = 0; index < 3; index++) {
+            checkValue += Character.getNumericValue(routingNumber.charAt(index * 3)) * 3;
+            checkValue += Character.getNumericValue(routingNumber.charAt(index * 3 + 1)) * 7;
+            checkValue += Character.getNumericValue(routingNumber.charAt(index * 3 + 2));
+        }
+
+        if (checkValue == 0 || checkValue % 10 != 0) {
+            throw new RequestValidationError(
+                    ErrorCodes.PAYMENT_1251.getCode());
+        }
+    }
