ead0ce5b6246ca478d077ba99ae6e847c4915cf1
erenKisaki <rakeshkisaki@gmail.com>
2026-02-04 22:09:31 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,195 +1,91 @@
-public String getInvalidFieldsFromReasonMessage(String reasonMessage) {
-    if (StringUtils.isBlank(reasonMessage)) {
-        return StringUtils.EMPTY;
-    }
+public Level2And3ProcessTransaction createLevelProcessTransactionObject(
+        LevelAuditTxn levelAuditTxn, String downgradeReason) {
 
-    List<String> invalidFieldReasons = extractInvalidFields(reasonMessage);
+    Level2And3ProcessTransaction processTxn = new Level2And3ProcessTransaction();
+    AuditCollectorRO auditCollectorRO =
+            new AuditCollectorRO(getGatewayTxn());
 
-    if (invalidFieldReasons.isEmpty()) {
-        return extractFallbackMessage(reasonMessage);
-    }
+    processTxn.setApiName(levelAuditTxn.getApiName());
 
-    return buildInvalidFieldMessage(invalidFieldReasons);
+    setShippingDetails(processTxn, levelAuditTxn);
+    setApiRequest(processTxn, levelAuditTxn);
+    setApiResponse(processTxn, levelAuditTxn);
+    processCardFeatures(processTxn, auditCollectorRO, levelAuditTxn);
+    setResponseCodes(processTxn, levelAuditTxn);
+
+    processTxn.setDowngradeReason(downgradeReason);
+    return processTxn;
 }
 
-private List<String> extractInvalidFields(String reasonMessage) {
-    List<String> results = new ArrayList<>();
-    String[] tokens = reasonMessage.split(INVALID_FIELD_TEXT);
+private void setShippingDetails(Level2And3ProcessTransaction processTxn,
+                                LevelAuditTxn levelAuditTxn) {
 
-    for (String token : tokens) {
-        if (!token.contains(SYMBOL_EQUAL_TO)) {
-            continue;
-        }
-
-        String value = token.substring(token.indexOf(SYMBOL_EQUAL_TO) + 1);
-        addParsedFields(results, value);
+    if (Objects.nonNull(levelAuditTxn.getShipTo())) {
+        processTxn.setShipToPostalCode(levelAuditTxn.getShipTo().getAddress().getZip());
+        processTxn.setShipToState(levelAuditTxn.getShipTo().getAddress().getState());
     }
-    return results;
 }
+private void setApiRequest(Level2And3ProcessTransaction processTxn,
+                           LevelAuditTxn levelAuditTxn) {
 
-private void addParsedFields(List<String> results, String value) {
-    String[] fields = value.contains(PaymentServiceConstants.EMPTY_SPACE)
-            ? value.split(PaymentServiceConstants.EMPTY_SPACE)
-            : new String[]{value};
-
-    for (String field : fields) {
-        if (StringUtils.isNotBlank(field)) {
-            results.add(cleanFieldName(field));
-        }
+    if (Objects.nonNull(levelAuditTxn.getRequest())) {
+        processTxn.setApiRequest(
+                loggingUtil.getObfuscatedJsonObject(levelAuditTxn.getRequest()));
     }
 }
 
-private String cleanFieldName(String field) {
-    StringBuilder cleaned = new StringBuilder();
+private void setApiResponse(Level2And3ProcessTransaction processTxn,
+                            LevelAuditTxn levelAuditTxn) {
 
-    for (int i = 0; i < field.length(); i++) {
-        char c = field.charAt(i);
-        if (!Character.isAlphabetic(c)) {
-            cleaned.append(c);
-        }
-    }
-    return StringUtils.deleteWhitespace(cleaned.toString());
-}
-
-private String extractFallbackMessage(String reasonMessage) {
-    String[] values = reasonMessage.contains(SYMBOL_HYPHEN)
-            ? reasonMessage.split(SYMBOL_HYPHEN)
-            : reasonMessage.split(SYMBOL_COLON);
-
-    return values.length > 1 ? StringUtils.deleteWhitespace(values[1]) : StringUtils.EMPTY;
-}
-
-private String buildInvalidFieldMessage(List<String> fields) {
-    return SYMBOL_HYPHEN + PaymentServiceConstant.EMPTY_SPACE +
-            String.join(CONNECTING_AND, fields);
-}
-
-
-// next class
-
-private String getCardTypeFromNetworkToken(String cardNumber) {
-    if (StringUtils.isBlank(cardNumber)) {
-        return null;
-    }
-
-    for (CardType cardType : CreditCardTypeEnum.getAll()) {
-        if (matchesCardType(cardType, cardNumber)) {
-            return cardType.getKey();
-        }
-    }
-
-    logNoCardType(cardNumber);
-    return null;
-}
-
-private boolean matchesCardType(CardType cardType, String cardNumber) {
-    if (CollectionUtils.isEmpty(cardType.getValue())) {
-        return false;
-    }
-
-    for (String prefix : cardType.getValue()) {
-        if (matchesPrefix(cardNumber, prefix)) {
-            return true;
-        }
-    }
-    return false;
-}
-
-private boolean matchesPrefix(String cardNumber, String prefix) {
-    String[] split = prefix.split("-");
-    String start = split[0];
-    String end = split.length > 1 ? split[1] : null;
-
-    if (cardNumber.startsWith(start)) {
-        return true;
-    }
-
-    return end != null && isBetween(cardNumber, start, end);
-}
-
-private boolean isBetween(String cardNumber, String start, String end) {
-    return cardNumber.substring(0, start.length()).compareTo(start) >= 0
-            && cardNumber.substring(0, end.length()).compareTo(end) <= 0;
-}
-
-private void logNoCardType(String cardNumber) {
-    LOGGER.error("No card type found for card number {}",
-            cardNumber.substring(0, Math.min(6, cardNumber.length())));
-}
-
-
-// next class
-
-private Level2And3ProcessTransaction buildProcessTxn(LevelAuditTxn auditTxn) {
-    Level2And3ProcessTransaction txn = new Level2And3ProcessTransaction();
-    mapRequest(txn, auditTxn);
-    mapResponse(txn, auditTxn);
-    mapCardFeatures(txn, auditTxn);
-    return txn;
-}
-
-private void mapRequest(Level2And3ProcessTransaction txn, LevelAuditTxn auditTxn) {
-    if (auditTxn.getRequest() != null) {
-        txn.setApiRequest(loggingUtil.getObfuscatedJsonObject(auditTxn.getRequest()));
+    if (Objects.nonNull(levelAuditTxn.getResponse())) {
+        processTxn.setApiResponse(
+                loggingUtil.getObfuscatedJsonObject(levelAuditTxn.getResponse()));
     }
 }
+private void processCardFeatures(Level2And3ProcessTransaction processTxn,
+                                 AuditCollectorRO auditCollectorRO,
+                                 LevelAuditTxn levelAuditTxn) {
 
-private void mapResponse(Level2And3ProcessTransaction txn, LevelAuditTxn auditTxn) {
-    if (auditTxn.getResponse() != null) {
-        txn.setApiResponse(loggingUtil.getObfuscatedJsonObject(auditTxn.getResponse()));
-    }
-}
-
-private void mapCardFeatures(Level2And3ProcessTransaction txn, LevelAuditTxn auditTxn) {
-    CardFeatures features = auditTxn.getCardFeatures();
-    if (features == null) {
+    CardFeatures cardFeatures = levelAuditTxn.getCardFeatures();
+    if (Objects.isNull(cardFeatures)) {
         return;
     }
 
-    txn.setCommercialCardIndicator(
-            StringUtils.defaultIfBlank(features.getCardCommerceIndicator(), null));
+    auditCollectorRO.setAffluenceIndicator(
+            toChar(cardFeatures.getAffluenceIndicator()));
 
-    txn.setHealthcare(
-            StringUtils.defaultIfBlank(features.getCardHealthcare(), null));
-
-    txn.setPayroll(
-            StringUtils.defaultIfBlank(features.getCardPayroll(), null));
-}
-
-
-// next class
-private Level2And3ProcessTransaction buildProcessTxn(LevelAuditTxn auditTxn) {
-    Level2And3ProcessTransaction txn = new Level2And3ProcessTransaction();
-    mapRequest(txn, auditTxn);
-    mapResponse(txn, auditTxn);
-    mapCardFeatures(txn, auditTxn);
-    return txn;
-}
-
-private void mapRequest(Level2And3ProcessTransaction txn, LevelAuditTxn auditTxn) {
-    if (auditTxn.getRequest() != null) {
-        txn.setApiRequest(loggingUtil.getObfuscatedJsonObject(auditTxn.getRequest()));
+    if (!IPConstants.SUCCESS_REASON_CODE.equals(levelAuditTxn.getReasonCode())) {
+        processTxn.setApiErrorCode(String.valueOf(levelAuditTxn.getReasonCode()));
+        processTxn.setApiErrorText(levelAuditTxn.getReasonMessage());
     }
-}
 
-private void mapResponse(Level2And3ProcessTransaction txn, LevelAuditTxn auditTxn) {
-    if (auditTxn.getResponse() != null) {
-        txn.setApiResponse(loggingUtil.getObfuscatedJsonObject(auditTxn.getResponse()));
-    }
+    auditCollectorRO.setCardCommercial(toChar(cardFeatures.getCardCommercial()));
+    auditCollectorRO.setCardHealthCare(toChar(cardFeatures.getCardHealthCare()));
+    auditCollectorRO.setCardIssuerCountry(toChar(cardFeatures.getCardIssuerCountry()));
+    auditCollectorRO.setCardLevel3Eligible(toChar(cardFeatures.getCardLevel3Eligible()));
+    auditCollectorRO.setCardPayroll(toChar(cardFeatures.getCardPayroll()));
+    auditCollectorRO.setCardPinlessDebit(toChar(cardFeatures.getCardPinlessDebit()));
+    auditCollectorRO.setCardPrepaid(toChar(cardFeatures.getCardPrepaid()));
+    auditCollectorRO.setCardRegulated(toChar(cardFeatures.getCardRegulated()));
+    auditCollectorRO.setCardSignatureDebit(toChar(cardFeatures.getCardSignatureDebit()));
 }
+private void setResponseCodes(Level2And3ProcessTransaction processTxn,
+                              LevelAuditTxn levelAuditTxn) {
 
-private void mapCardFeatures(Level2And3ProcessTransaction txn, LevelAuditTxn auditTxn) {
-    CardFeatures features = auditTxn.getCardFeatures();
-    if (features == null) {
+    if (StringUtils.isNotBlank(levelAuditTxn.getProcessorResponseCode())) {
+        processTxn.setProcessorResponseCode(
+                levelAuditTxn.getProcessorResponseCode());
         return;
     }
 
-    txn.setCommercialCardIndicator(
-            StringUtils.defaultIfBlank(features.getCardCommerceIndicator(), null));
+    if (StringUtils.isNotBlank(levelAuditTxn.getCaptureResponseCode())) {
+        processTxn.setProcessorResponseCode(
+                levelAuditTxn.getCaptureResponseCode());
+    }
 
-    txn.setHealthcare(
-            StringUtils.defaultIfBlank(features.getCardHealthcare(), null));
-
-    txn.setPayroll(
-            StringUtils.defaultIfBlank(features.getCardPayroll(), null));
+    processTxn.setLevel3Enabled(
+            toChar(levelAuditTxn.getPurchasingLevel3Enabled()));
+}
+private Character toChar(String value) {
+    return StringUtils.isNotEmpty(value) ? value.charAt(0) : null;
 }
