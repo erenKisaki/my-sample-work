b60e4e4a2174d21982b453673f6d82c6da446baf
erenKisaki <rakeshkisaki@gmail.com>
2026-02-27 22:51:46 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,4 +1,6 @@
-var tokenRegexPatterns =
-        Optional.ofNullable(configuration)
-            .map(c -> c.getStringList("app.xbucks.token.regex"))
-            .orElse(Collections.emptyList());
+if (StringUtils.isNotBlank(expiryDate) 
+            && expiryDate.length() == 4 
+            && StringUtils.isNumeric(expiryDate)) {
+pymtGtwyTxn.setNetworkTokenExpirationMonth(expiryDate.substring(0, 2));
+            pymtGtwyTxn.setNetworkTokenExpirationYear(expiryDate.substring(2, 4));
+}
