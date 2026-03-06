796acce11223d12b31156bcf1afa82578ef3d6a3
erenKisaki <rakeshkisaki@gmail.com>
2026-03-07 00:38:43 +0530

    Refactor expiry date handling with Stream API

--- Partition.java
+++ Partition.java
@@ -1,6 +1,5 @@
-if (StringUtils.isNotBlank(expiryDate) 
-            && expiryDate.length() == 4 
-            && StringUtils.isNumeric(expiryDate)) {
-pymtGtwyTxn.setNetworkTokenExpirationMonth(expiryDate.substring(0, 2));
-            pymtGtwyTxn.setNetworkTokenExpirationYear(expiryDate.substring(2, 4));
-}
+String expDate = Stream.of(str1, str2)
+        .filter(Objects::nonNull)
+        .collect(Collectors.joining());
+
+expDate = expDate.isEmpty() ? null : expDate;
