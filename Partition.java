033bbd9d55e50042385bac0c99bed4862210512c
erenKisaki <rakeshkisaki@gmail.com>
2026-03-13 18:53:00 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,3 +1,35 @@
-var networkMonth = 
-var networkYear = 
-String expDate = (StringUtils.isNotEmpty(networkMonth) &&  StringUtils.isNotEmpty(networkYear)) ? (networkMonth + networkYear) : null;
+private LocalDate getRunDateSkippingWeekend(LocalDate runDate) {
+
+    long saturdayLogic = getLongProperty(CUSTOM_REAUTH_SATURDAY_LOGIC, 1L);
+    long sundayLogic = getLongProperty(CUSTOM_REAUTH_SUNDAY_LOGIC, 1L);
+
+    LocalDate candidateDate = runDate;
+
+    while (true) {
+
+        DayOfWeek day = candidateDate.getDayOfWeek();
+
+        if (day == DayOfWeek.SATURDAY) {
+
+            if (saturdayLogic == 0) {
+                return candidateDate;
+            }
+
+            candidateDate = candidateDate.plusDays(saturdayLogic);
+            continue;
+        }
+
+        if (day == DayOfWeek.SUNDAY) {
+
+            if (sundayLogic == 0) {
+                return candidateDate;
+            }
+
+            candidateDate = candidateDate.plusDays(sundayLogic);
+            continue;
+        }
+
+        // weekday
+        return candidateDate;
+    }
+}
