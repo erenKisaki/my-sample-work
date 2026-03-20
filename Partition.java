699a41d454f5cc33007c5996c6dd54f595035037
erenKisaki <rakeshkisaki@gmail.com>
2026-03-20 21:16:14 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,15 +1,3 @@
-public static boolean isValuePresentInProperties(String input,
-                                                 Properties propertiesValidationLabelUMS) {
-    if (input == null || propertiesValidationLabelUMS == null) {
-        return false;
-    }
-
-    String normalizedInput = input.trim().toLowerCase();
-
-    return propertiesValidationLabelUMS.values().stream()
-            .filter(Objects::nonNull)
-            .map(Object::toString)
-            .map(String::trim)
-            .map(String::toLowerCase)
-            .anyMatch(val -> val.equals(normalizedInput));
-}
+ResponseEntity
+        .status(status != null ? status : HttpStatus.INTERNAL_SERVER_ERROR)
+        .build();
