e8fb0ef685de2bcd4bcfc8cc42c13ea078936208
erenKisaki <rakeshkisaki@gmail.com>
2026-03-20 15:13:58 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,2 +1,20 @@
-By.xpath("//div[contains(text(),'Application')]/following::select[1]")
-By.xpath("//form[@id='manageTokenForm']//div[text()='Token Status']/following::select[1]")
+public static boolean validateDropdownOptionsByLabelMap(Select dropdown,
+                                                        Properties propertiesValidationLabelUMS) {
+    try {
+        // Convert properties values to lowercase Set for fast lookup
+        Set<String> propertyValues = propertiesValidationLabelUMS.values().stream()
+                .filter(Objects::nonNull)
+                .map(Object::toString)
+                .map(String::trim)
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
+
+        // Check all dropdown options exist in properties
+        return dropdown.getOptions().stream()
+                .map(option -> option.getText().trim().toLowerCase())
+                .allMatch(propertyValues::contains);
+
+    } catch (Exception e) {
+        return false;
+    }
+}
