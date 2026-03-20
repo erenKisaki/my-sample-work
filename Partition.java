7889be61237ff33ad1eea209f2e36cc12d98dbfa
erenKisaki <rakeshkisaki@gmail.com>
2026-03-20 15:40:14 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,20 +1,15 @@
-public static boolean validateDropdownOptionsByLabelMap(Select dropdown,
-                                                        Properties propertiesValidationLabelUMS) {
-    try {
-        // Convert properties values to lowercase Set for fast lookup
-        Set<String> propertyValues = propertiesValidationLabelUMS.values().stream()
-                .filter(Objects::nonNull)
-                .map(Object::toString)
-                .map(String::trim)
-                .map(String::toLowerCase)
-                .collect(Collectors.toSet());
-
-        // Check all dropdown options exist in properties
-        return dropdown.getOptions().stream()
-                .map(option -> option.getText().trim().toLowerCase())
-                .allMatch(propertyValues::contains);
-
-    } catch (Exception e) {
+public static boolean isValuePresentInProperties(String input,
+                                                 Properties propertiesValidationLabelUMS) {
+    if (input == null || propertiesValidationLabelUMS == null) {
         return false;
     }
+
+    String normalizedInput = input.trim().toLowerCase();
+
+    return propertiesValidationLabelUMS.values().stream()
+            .filter(Objects::nonNull)
+            .map(Object::toString)
+            .map(String::trim)
+            .map(String::toLowerCase)
+            .anyMatch(val -> val.equals(normalizedInput));
 }
