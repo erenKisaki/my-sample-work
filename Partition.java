020e57b34b05fe02a75b7177252d218d9c382df2
erenKisaki <rakeshkisaki@gmail.com>
2026-04-02 14:58:11 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,26 +1,7 @@
-if (StringUtils.isNotBlank(errorMessage)) {
+String message = errorMessage.toLowerCase();
 
-    var retryConfigurationMessageList =
-            configuration.getStringList(CpxConstants.CPX_RETRY_ERROR_MESSAGE_KEY + clientKey);
-
-    // If no config → do not retry
-    if (CollectionUtils.isEmpty(retryConfigurationMessageList)) {
-        return false;
-    }
-
-    boolean isRetryConfigurationMessage = retryConfigurationMessageList.stream()
-            .filter(Objects::nonNull)
-            .map(String::trim)
-            .filter(s -> !s.isEmpty())
-            .anyMatch(k ->
-                    Optional.ofNullable(errorMessage)
-                            .map(String::toLowerCase)
-                            .orElse("")
-                            .contains(k.toLowerCase())
-            );
-
-    // If errorMessage exists but doesn't match any configured values → return false
-    if (!isRetryConfigurationMessage) {
-        return false;
-    }
-}
+boolean isRetryConfigurationMessage = retryConfigurationMessageList.stream()
+    .filter(Objects::nonNull)
+    .map(String::trim)
+    .filter(s -> !s.isEmpty())
+    .anyMatch(k -> message.contains(k.toLowerCase()));
