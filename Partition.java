6ed6fb8d0f403bbbe44dea84c4e935b758e89732
erenKisaki <rakeshkisaki@gmail.com>
2026-04-10 01:20:51 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,7 +1,2 @@
-String message = errorMessage.toLowerCase();
-
-boolean isRetryConfigurationMessage = retryConfigurationMessageList.stream()
-    .filter(Objects::nonNull)
-    .map(String::trim)
-    .filter(s -> !s.isEmpty())
-    .anyMatch(k -> message.contains(k.toLowerCase()));
+when(environment.getProperty(anyString(), eq(Boolean.class), anyBoolean()))
+        .thenAnswer(invocation -> invocation.getArgument(2));
