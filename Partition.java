6086d5342dd0953806b96b54d8127e87f7b36944
erenKisaki <rakeshkisaki@gmail.com>
2026-04-14 00:12:38 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,10 +1,4 @@
-Calendar cal = Calendar.getInstance();
-cal.setTime(reAuthDate);
+long time = reAuthDate.getTime();
+TimeZone tz = TimeZone.getDefault();
 
-cal.set(Calendar.HOUR_OF_DAY, 0);
-cal.set(Calendar.MINUTE, 0);
-cal.set(Calendar.SECOND, 0);
-cal.set(Calendar.MILLISECOND, 0);
-
-// update SAME object
-reAuthDate.setTime(cal.getTimeInMillis());
+reAuthDate.setTime(time - (time + tz.getOffset(time)) % (24 * 60 * 60 * 1000));
