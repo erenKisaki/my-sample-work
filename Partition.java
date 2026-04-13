13b482a9d4bac344d612ca8f2e8b880db39ef7f9
erenKisaki <rakeshkisaki@gmail.com>
2026-04-14 00:11:21 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,2 +1,10 @@
- @DirtiesContext
-    @TestPropertySource(properties = "feature.enabled=true")
+Calendar cal = Calendar.getInstance();
+cal.setTime(reAuthDate);
+
+cal.set(Calendar.HOUR_OF_DAY, 0);
+cal.set(Calendar.MINUTE, 0);
+cal.set(Calendar.SECOND, 0);
+cal.set(Calendar.MILLISECOND, 0);
+
+// update SAME object
+reAuthDate.setTime(cal.getTimeInMillis());
