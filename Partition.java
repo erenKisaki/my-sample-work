314c1902c9f7b238c0a64c4a39f7cfc6d605f001
erenKisaki <rakeshkisaki@gmail.com>
2026-05-12 15:01:11 +0530

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,2 +1,11 @@
-ObjectMapper mapper = new ObjectMapper();
-String json = mapper.writeValueAsString(obj);
+// Username
+driver.findElement(By.xpath("//input[@id='input-user-name']")).sendKeys("sample");
+
+// Password
+driver.findElement(By.xpath("//input[@id='input-password-login']")).sendKeys("password");
+
+// Token value
+driver.findElement(By.xpath("//input[@id='input-token']")).sendKeys("123456");
+
+// Sign in button
+driver.findElement(By.xpath("//button[@id='btn-login-pass']")).click();
